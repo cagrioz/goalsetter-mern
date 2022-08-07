7c39ecdb8c4e3a6e2cdd92dcfe0f56af48f94b73
Cagri Oz <cagriozarpaciii@hotmail.com>
2022-08-07 18:30:40 +0300

    backend fixes

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -42,10 +42,8 @@ const updateGoal = asyncHandler(async (req, res) => {
         throw new Error('Goal not found!');
     }
 
-    const user = await User.findById(req.user.id);
-
     // Check for user
-    if (!user) {
+    if (!req.user) {
         res.status(401);
         throw new Error('User not found!');
     }
@@ -74,10 +72,8 @@ const deleteGoal = asyncHandler(async (req, res) => {
         throw new Error('Goal not found!');
     }
 
-    const user = await User.findById(req.user.id);
-
     // Check for user
-    if (!user) {
+    if (!req.user) {
         res.status(401);
         throw new Error('User not found!');
     }
